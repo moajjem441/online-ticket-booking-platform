f7d4724f91a78d78dd0d720459b8c4978358be17
Moajjem Hossain <moajjem441@gmail.com>
2026-06-24 02:22:27 +0600

    latest ticket section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import AdvertisementSection from "@/components/AdvertisementSection";
 import Banner from "@/components/Banner";
 import LatestTicketsSection from "@/components/LatestTicketsSection";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
